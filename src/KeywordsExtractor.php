016f0160d4ef5874667bfb23ceed9209c4ddadbb
Sergey Kudashev <kudashevs@gmail.com>
2025-01-25 13:54:19 +0300

    Style KeywordsExtractor add some docblocks

--- src/KeywordsExtractor.php
+++ src/KeywordsExtractor.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kudashevs\KeywordsExtractor;
 
+use InvalidArgumentException;
 use Kudashevs\KeywordsExtractor\Exceptions\InvalidOptionType;
 use Kudashevs\KeywordsExtractor\Extractors\Extractor;
 use Kudashevs\KeywordsExtractor\Extractors\RakeExtractor;
@@ -19,6 +20,17 @@ class KeywordsExtractor
         'remove' => [],
     ];
 
+    /**
+     * 'add_words'      string|array A string or an array with words to add to the result (if they are ignored by an Extractor).
+     * 'remove_words'   string|array An string or an array with words to remove from the result (if they are not ignored by an Extractor).
+     *
+     * @param array{
+     *     add_words?: string|array<array-key, string>,
+     *     remove_words?: string|array<array-key, string>,
+     * } $options
+     *
+     * @throws InvalidArgumentException
+     */
     public function __construct(array $options = [])
     {
         $this->initOptions($options);
@@ -26,12 +38,18 @@ class KeywordsExtractor
         $this->initExtractor();
     }
 
+    /**
+     * @param array<array-key, string|array<array-key, string>> $options
+     */
     protected function initOptions(array $options): void
     {
         $this->initAddOption($options);
         $this->initRemoveOption($options);
     }
 
+    /**
+     * @param array<array-key, string|array<array-key, string>> $options
+     */
     protected function initAddOption(array $options): void
     {
         if (
@@ -47,6 +65,9 @@ class KeywordsExtractor
             : $options['add_words'];
     }
 
+    /**
+     * @param array<array-key, string|array<array-key, string>> $options
+     */
     protected function initRemoveOption(array $options): void
     {
         if (
