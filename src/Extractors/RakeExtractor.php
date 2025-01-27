f855bc22bdcbc1bc9ce2eb22f41115a71f560323
Sergey Kudashev <kudashevs@gmail.com>
2025-01-27 17:15:34 +0300

    Style RakeExtractor add some docblocks

--- src/Extractors/RakeExtractor.php
+++ src/Extractors/RakeExtractor.php
@@ -10,6 +10,12 @@ final class RakeExtractor implements Extractor
 {
     private Rake $extractor;
 
+    /**
+     * @param array{
+     *     add?: array<array-key, string>,
+     *     remove?: array<array-key, string>,
+     *} $options
+     */
     public function __construct(array $options = [])
     {
         $this->extractor = new Rake([
@@ -30,6 +36,9 @@ final class RakeExtractor implements Extractor
         return array_keys($keywords);
     }
 
+    /**
+     * @param array<array-key, string> $words
+     */
     public function addWords(array $words): void
     {
         $this->extractor = $this->cloneAddWords($words);
@@ -53,6 +62,9 @@ final class RakeExtractor implements Extractor
         return $newWithAddedWords;
     }
 
+    /**
+     * @param array<array-key, string> $words
+     */
     public function removeWords(array $words): void
     {
         $this->extractor = $this->cloneRemoveWords($words);
