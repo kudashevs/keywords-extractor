76753cb15617d4d2d468295f0fa70f803aaa58b8
Sergey Kudashev <kudashevs@gmail.com>
2025-01-27 17:11:45 +0300

    Update Extractor clarify the abstraction.

--- src/Extractors/Extractor.php
+++ src/Extractors/Extractor.php
@@ -10,12 +10,20 @@ namespace Kudashevs\KeywordsExtractor\Extractors;
 interface Extractor
 {
     /**
-     * Extract keywords from a text.
+     * Extract keywords from a text in a format keyword => weigh.
      *
      * @param string $text
      * @return array<string, int|float>
      */
     public function extract(string $text): array;
+
+    /**
+     * Extract keywords from a text.
+     *
+     * @param string $text
+     * @return array<array-key, string>
+     */
+    public function extractWords(string $text): array;
 
     /**
      * An array of words to add to the result.
