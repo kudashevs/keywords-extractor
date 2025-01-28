8891e9ba56ea5c8e6f387258f58de0796a4981c0
Sergey Kudashev <kudashevs@gmail.com>
2025-01-28 11:53:39 +0300

    Update RakeExtractorTest add some adverbs-related tests

--- tests/Unit/Extractors/RakeExtractorTest.php
+++ tests/Unit/Extractors/RakeExtractorTest.php
@@ -8,13 +8,34 @@ use PHPUnit\Framework\TestCase;
 
 class RakeExtractorTest extends TestCase
 {
+    private RakeExtractor $extractor;
+
+    protected function setUp(): void
+    {
+        $this->extractor = new RakeExtractor();
+    }
+
     #[Test]
     public function it_can_extract_keywords(): void
     {
-        $extractor = new RakeExtractor();
-
-        $keywords = $extractor->extractWords('cool, this is a test');
+        $keywords = $this->extractor->extractWords('cool, this is a test');
 
         $this->assertCount(2, $keywords);
+    }
+
+    #[Test]
+    public function it_can_remove_an_adverb_with_ly(): void
+    {
+        $keywords = $this->extractor->extractWords('warningly this is a test');
+
+        $this->assertCount(1, $keywords);
+    }
+
+    #[Test]
+    public function it_can_remove_other_adverbs(): void
+    {
+        $keywords = $this->extractor->extractWords('almost now this is a test');
+
+        $this->assertCount(1, $keywords);
     }
 }
