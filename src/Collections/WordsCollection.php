d934ec8c3e2ad199382b68333fc192dd94a7d76b
Sergey Kudashev <kudashevs@gmail.com>
2025-01-31 12:43:59 +0300

    Update Collections refine the words collection abstraction

--- src/Collections/WordsCollection.php
+++ src/Collections/WordsCollection.php
@@ -5,10 +5,17 @@ declare(strict_types=1);
 namespace Kudashevs\KeywordsExtractor\Collections;
 
 /**
- * WordsCollection represents an abstraction with a collection of words lists.
+ * WordsCollection represents an abstraction with a named collection of words lists.
  */
 interface WordsCollection
 {
+    /**
+     * Get a unique collection name.
+     *
+     * @return string
+     */
+    public function getName(): string;
+
     /**
      * Get a list of words.
      *
